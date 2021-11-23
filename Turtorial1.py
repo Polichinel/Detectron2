cbc30fc2bc5d302186a4b65cce194840c2536fc3
Polichinel <spm@ifs.ku.dk>
2021-11-23 16:09:26 +0100

    first try with traning

--- Turtorial1.py
+++ Turtorial1.py
@@ -54,3 +54,61 @@ viz_im = out.get_image()[:, :, ::-1]
 print(type(viz_im))
 viz_im_path = './Turtorial1_test1.jpg'
 cv2.imwrite(viz_im_path, viz_im)
+print('Turtorial1_test1.jpg saved...')
+
+
+# --------------------------------------------------------------------------------------
+# Retraining on costum dataset (my own?)
+# somewhat sim to pytorch dataloader...
+print('starting retraining...')
+
+from detectron2.structures import BoxMode
+from Turtorial1_utils import *
+
+# home dir
+#img_dir = '/home/simon/Documents/Bodies/data/jeppe/images'
+
+# computerome dir
+img_dir = '/home/projects/ku_00017/data/raw/bodies/OD_images_annotated'
+
+#dataset_dicts = get_img_dicts(img_dir) # you don't need to load it here.. I thinks..
+
+# you do still not have dedicated train and test set. is it about time you did?
+# meybe also move it out of the jeppe dir..
+
+DatasetCatalog.register("bodies_OD_data", lambda: get_img_dicts(img_dir))
+#MetadataCatalog.get("my_data").set(thing_classes=classes) # alt
+MetadataCatalog.get("bodies_OD_data").thing_classes=classes
+bodies_OD_metadata = MetadataCatalog.get("bodies_OD_data")
+
+print('data registered')
+
+from detectron2.engine import DefaultTrainer
+
+classes, _ , _ = get_classes(img_dir)
+n_classes = len(classes)
+
+# -------------------------
+
+cfg = get_cfg()
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("bodies_OD_data",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 2
+cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+cfg.SOLVER.MAX_ITER = 300    # you will need to train longer for a practical dataset
+cfg.SOLVER.STEPS = []        # do not decay learning rate
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   #  128 would be faster (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = n_classes  #(see https://detectron2.readthedocs.io/tutorials/datasets.html#update-the-config-for-new-datasets)
+#note: this config means the number of classes, but a few popular unofficial tutorials incorrect uses num_classes+1 here.
+
+print('modle loaded and hyper parameters set.')
+print('beginning traning')
+
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = DefaultTrainer(cfg) 
+trainer.resume_or_load(resume=False)
+trainer.train()
