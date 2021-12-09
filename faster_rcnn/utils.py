1fac41d0c9ac2f97d7746eae0837851e8239a056
Polichinel <spm@ifs.ku.dk>
2021-12-09 09:10:46 +0100

    trying w/  LossEvalHook

--- faster_rcnn/utils.py
+++ faster_rcnn/utils.py
@@ -10,6 +10,7 @@ from detectron2 import model_zoo
 from detectron2.data import MetadataCatalog, DatasetCatalog
 from detectron2.engine import DefaultTrainer
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from LossEvalHook import *
 
 
 
@@ -231,3 +232,16 @@ class MyTrainer(DefaultTrainer):
         if output_folder is None:
             output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
         return COCOEvaluator(dataset_name, cfg, True, output_folder)
+                     
+    def build_hooks(self):
+        hooks = super().build_hooks()
+        hooks.insert(-1,LossEvalHook(
+            cfg.TEST.EVAL_PERIOD,
+            self.model,
+            build_detection_test_loader(
+                self.cfg,
+                self.cfg.DATASETS.TEST[0],
+                DatasetMapper(self.cfg,True)
+            )
+        ))
+        return hooks
