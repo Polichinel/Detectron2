8583c29fc7d56dc1ac5b62ebe647dc1e1074f089
Polichinel <spm@ifs.ku.dk>
2022-02-03 11:14:12 +0100

    added correct data path

--- faster_rcnn_X_101_32x8d_FPN_3x/prediction.py
+++ faster_rcnn_X_101_32x8d_FPN_3x/prediction.py
@@ -14,19 +14,17 @@ with open(cfg_pkl_path, 'rb') as file:
     cfg = pickle.load(file)
  
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1   # set a custom testing threshold
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1  # set a custom testing threshold
 
 predictor = DefaultPredictor(cfg)
 
-
-# now this should not be annotated images but all... so you need to get all images in a flat folder to computerome...
-img_dir = '/home/projects/ku_00017/data/raw/bodies/...' #  '/home/simon/Documents/Bodies/data/jeppe/images'
+img_dir = '/home/projects/ku_00017/data/raw/bodies/images_johan'
 
 img_path_list = get_img_path(img_dir)
 
 outputs_list = []
 
-for img_path in img_path_list:
+for img_path in img_path_list[0:5]: #the five first as a test.
 
         im = cv2.imread(img_path)
         outputs = predictor(im)
