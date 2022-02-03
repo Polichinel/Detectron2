908488f5fe233554894750a934dd1efff92f2a89
Polichinel <spm@ifs.ku.dk>
2022-02-03 23:25:11 +0100

    10k test

--- faster_rcnn_X_101_32x8d_FPN_3x/prediction.py
+++ faster_rcnn_X_101_32x8d_FPN_3x/prediction.py
@@ -16,28 +16,32 @@ with open(cfg_pkl_path, 'rb') as file:
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.1  # set a custom testing threshold
 
+
 predictor = DefaultPredictor(cfg)
 
 img_dir = '/home/projects/ku_00017/data/raw/bodies/images_spanner'
 
 img_path_list = get_img_path(img_dir)
-print(img_path_list[0:10])
+#print(img_path_list[0:10])
 
 outputs_list = []
+total_count = 10000 #len(img_path_list)
 
-for img_path in img_path_list[0:10]: #the five first as a test.
+for count, img_path in enumerate(img_path_list[0:total_count]): #10000
 
-        print(img_path)
+        #print(img_path)
 
         im = cv2.imread(img_path)
         outputs = predictor(im)
-        print(outputs)
+        #print(outputs)
 
         outputs['im_id'] = img_path.split('/')[-1].split('0')[0]
-        print(outputs)
+        #print(outputs)
 
         outputs_list.append(outputs["instances"].to("cpu"))
-        print(outputs)
+        #print(outputs)
+
+        print(f'{count} of {total_count} done', '\r')
 
 # pickle configurations - where do you want to save this?
 with open(f'outputs_list.pkl', 'wb') as file:
