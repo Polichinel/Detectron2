0342528c370e83b93b96365d89662307e48af592
Polichinel <spm@ifs.ku.dk>
2022-02-10 10:14:35 +0100

    better outputs

--- faster_rcnn_X_101_32x8d_FPN_3x/prediction.py
+++ faster_rcnn_X_101_32x8d_FPN_3x/prediction.py
@@ -7,6 +7,9 @@ import os
 import pickle
 from utils import *
 
+from collections import Counter
+import pandas as pd
+
 cfg_pkl_path = 'faster_rcnn_X_101_32x8d_FPN_3x.pkl' # path to the config file you just created
 model_name = "faster_rcnn_X_101_32x8d_FPN_3x"
 
@@ -24,25 +27,66 @@ img_dir = '/home/projects/ku_00017/data/raw/bodies/images_spanner'
 img_path_list = get_img_path(img_dir)
 #print(img_path_list[0:10])
 
-outputs_list = []
+# ---------------------------
+classes, classes_int, class_to_int = get_classes(img_dir)
+int_to_class = dict(zip(classes_int,classes)) # dict to translate from int encoding of feature to str of feature name.
+# ---------------------------
+
+# containers:
+instances_list = []
+output_list = []
+all_img_feature_list = [] # to create the slim df, its easier this way...
+
+# number of images to predict
 total_count = 10000 #len(img_path_list)
 
+# prediction loop
 for count, img_path in enumerate(img_path_list[0:total_count]): #10000
 
-        #print(img_path)
 
         im = cv2.imread(img_path)
-        outputs = predictor(im)
-        #print(outputs)
+        output = predictor(im)
+        instance = output["instances"].to("cpu")
+        instances_list.append(instance) # nice to have for backup and checks
 
-        outputs['im_id'] = img_path.split('/')[-1].split('0')[0]
-        #print(outputs)
 
-        outputs_list.append(outputs["instances"].to("cpu"))
-        #print(outputs)
+        # more manageable output format ---------------------------------------------------------------------
+        img_id = img_path.split('/')[-1].split('.')[0]
+        img_dict = {'img_id': img_id, 'scores': None , 'pred_classes': None}
+        img_dict['scores'] = instance.scores.numpy()
+        img_dict['pred_classes'] = instance.pred_classes.numpy()
 
-        print(f'{count} of {total_count} done', '\r')
+        img_feature_Int_count = dict(Counter(instance.pred_classes.numpy())) # counting the classes - int encoded
+
+        img_dict = {**img_dict, **img_feature_Int_count} # merging counts with other info 
+
+        img_feature_list = [int_to_class[i] for i in instance.pred_classes.numpy()] # convert from int encoded feature to str of feature name
+        img_feature_count = dict(Counter(img_feature_list)) # count the actual feature name
+
+        img_dict = {**img_dict, **img_feature_count} # merging counts name with other info - actual feature 
+        
+        output_list.append(img_dict)
+        all_img_feature_list += img_feature_list #this will just be a list of all encountered features..
+
+        print(f'img id: {img_id}, {count} of {total_count} done', end = '\r')
+
+
+# Outputs:
+all_img_feature_list = list(set(all_img_feature_list)) # get the unique set of features - nice for the thin df.
+
+df_thick = pd.DataFrame(output_list).fillna(0) # NaN countes to 0
+df_thick.iloc[:,3:] = df_thick.iloc[:,3:].astype('int') # counts as ints - not floats 
+df_thin = df_thick[['img_id'] + all_img_feature_list].copy() # making a df with just feature counts and img_id
 
 # pickle configurations - where do you want to save this?
-with open(f'outputs_list.pkl', 'wb') as file:
-    pickle.dump(outputs_list, file)
+location = '' # computerome date gen
+
+# A bit overkill, but nice for checking errors down the line.
+with open(f'instances_list.pkl', 'wb') as file:
+    pickle.dump(instances_list, file)
+
+with open(f'df_thick.pkl', 'wb') as file:
+    pickle.dump(df_thick, file)
+
+with open(f'df_thin.pkl', 'wb') as file:
+    pickle.dump(df_thin, file)
