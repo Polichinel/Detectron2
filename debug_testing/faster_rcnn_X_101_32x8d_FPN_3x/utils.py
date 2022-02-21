6a8aff996ae32e85869d047e64b1c6205aa6bfb3
Polichinel <spm@ifs.ku.dk>
2022-02-21 23:50:22 +0100

    removed testing...

--- debug_testing/faster_rcnn_X_101_32x8d_FPN_3x/utils.py
+++ debug_testing/faster_rcnn_X_101_32x8d_FPN_3x/utils.py
@@ -210,8 +210,8 @@ def get_train_cfg(config_file_path, checkpoint_url, train_data, test_data, outpu
     
     # new ------------------
     #cfg.DATASETS.TEST = ()
-    cfg.DATASETS.TEST = (test_data,) # test data needs to be input
-    cfg.TEST.EVAL_PERIOD = 100
+    # cfg.DATASETS.TEST = (test_data,) # test data needs to be input
+    # cfg.TEST.EVAL_PERIOD = 100
     # tjekc what this does before adding more jazz.
 
     # ---------------------------------
