4004f340a44d82d9c18ede5bc3ac251e2b071f31
Polichinel <spm@ifs.ku.dk>
2022-02-22 00:41:27 +0100

    working?

--- full_run_no_test/faster_rcnn_R_101_FPN_3x_FULL/utils_FULL.py
+++ full_run_no_test/faster_rcnn_R_101_FPN_3x_FULL/utils_FULL.py
@@ -256,16 +256,16 @@ class MyTrainer(DefaultTrainer):
 
         return build_detection_train_loader(cfg, mapper=train_mapper)
 
-    @classmethod
-    def build_evaluator(cls, cfg, dataset_name, output_folder=None):
-        if output_folder is None:
-            output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
-        # return COCOEvaluator(dataset_name, cfg, True, output_folder) 
-        # Regarding second argument, task = cfg:
-        # COCO Evaluator instantiated using config, this is deprecated be havior. Please pass in explicit arguments instead.
-        # Tasks (tuple[str]) – tasks that can be evaluated under the given configuration. A task is one of “bbox”, “segm”, “keypoints”.
-        # Note: By default, will infer this automatically from predictions.
-        return COCOEvaluator(dataset_name, ('bbox',), True, output_folder)
+    # @classmethod
+    # def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+    #     if output_folder is None:
+    #         output_folder = os.path.join(cfg.OUTPUT_DIR, "inference")
+    #     # return COCOEvaluator(dataset_name, cfg, True, output_folder) 
+    #     # Regarding second argument, task = cfg:
+    #     # COCO Evaluator instantiated using config, this is deprecated be havior. Please pass in explicit arguments instead.
+    #     # Tasks (tuple[str]) – tasks that can be evaluated under the given configuration. A task is one of “bbox”, “segm”, “keypoints”.
+    #     # Note: By default, will infer this automatically from predictions.
+    #     return COCOEvaluator(dataset_name, ('bbox',), True, output_folder)
                      
     # def build_hooks(self):
 
