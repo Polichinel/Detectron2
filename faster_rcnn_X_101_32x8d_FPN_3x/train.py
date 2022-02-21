89c661fce2f1047da9ca805f039b7204cb56aeaf
Polichinel <spm@ifs.ku.dk>
2022-02-21 08:49:09 +0100

    1k test w/ fixed train/test set

--- faster_rcnn_X_101_32x8d_FPN_3x/train.py
+++ faster_rcnn_X_101_32x8d_FPN_3x/train.py
@@ -48,7 +48,7 @@ num_worker = 2
 img_per_batch = 2
 learning_rate = 0.00025
 decay_LR = []
-max_iter =  120000
+max_iter =  1000 #120000
 
 print(f'running for {max_iter} iterations. Learing rate: {learning_rate}, Image per batch: {img_per_batch}')
 
