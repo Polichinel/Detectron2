bc606539454e5b599f0c2818526c4a5ae5af421d
Polichinel <spm@ifs.ku.dk>
2022-06-28 09:54:56 +0200

    need to do exposure first

---
+++ misc/sjoin.py
@@ -0,0 +1,10 @@
+# use geo_env_2022
+import os
+import numpy as np
+import pandas as pd # original 1.2.3
+import geopandas as gpd
+from shapely.geometry import Point
+import pickle
+import urllib.request
+
+import matplotlib.pyplot as plt
