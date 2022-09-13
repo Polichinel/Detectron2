83fc46f18bf104c07310f3e3dbaa3ac595b9cb40
Polichinel <spm@ifs.ku.dk>
2022-09-13 15:08:11 +0200

    first commit

---
+++ misc/timelaps_y_prio.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+import pandas as pd # original 1.2.3
+import pickle
+import matplotlib.pyplot as plt
+
+
+def get_data():
+
+    data_dir = '/home/simon/Documents/Bodies/data/PRIO'
+
+    with open(f'{data_dir}/yearly_prio_interpl.pkl', 'rb') as file:
+        df = pickle.load(file)
+
+    return(df)
+
+
+def plot_maps(df):
+
+    map_dir = '/home/simon/Documents/Bodies/figures/maps/PRIO_timelaps/'
+
+    for feature in df.columns[3:4]:#[2:-20]:
+
+        feature_dir = os.path.join(map_dir, feature)
+        os.makedirs(feature_dir, exist_ok = True)
+
+        for month_id in df['month_id'].unique():
+
+            x = df.loc[df['month_id'] == month_id, 'xcoord'] 
+            y = df.loc[df['month_id'] == month_id, 'ycoord'] 
+            c = df.loc[df['month_id'] == month_id, feature] 
+
+            plt.figure(figsize=[28,12])
+            plt.scatter(x, y, c = c, cmap = 'rainbow', marker = 's', s = 2, vmin= c.min(), vmax=c.max())
+        
+            fig_title = f'{feature}_{str(month_id).zfill(3)}'
+
+            plt.title(fig_title)
+            plt.savefig(feature_dir + '/' + fig_title + '.JPG', bbox_inches="tight")
+            plt.cla() # idk if this is also needed..
+            plt.close('all') # so they do not display or take up mem
+
+def make_maps():
+    df = get_data()
+    plot_maps(df)
+
+if __name__ == '__main__':
+    make_maps()
