638420912ec73588571057dd92abebc58cf13495
Polichinel <spm@ifs.ku.dk>
2022-10-27 01:18:28 +0200

    0.2

--- full_run_johan_pub/retinanet_R_101_FPN_3x_FULL/prediction_FULL.py
+++ full_run_johan_pub/retinanet_R_101_FPN_3x_FULL/prediction_FULL.py
@@ -21,7 +21,7 @@ cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to t
 #cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.9 # 0.1 CHANGE back!!!!  # set a custom testing threshold
 #cfg.MODEL.ROI_HEADS.NMS_THRESH_TEST = 0.9 # new
 
-cfg.MODEL.RETINANET.SCORE_THRESH_TEST = 0.25
+cfg.MODEL.RETINANET.SCORE_THRESH_TEST = 0.2
 
 predictor = DefaultPredictor(cfg)
 
